3d6830af34340dad0ad6e1c6c582677517e5d221
Aleksandra Sumatokhina <sneginca.cat@gmail.com>
2024-11-03 21:16:25 +0500

    day 1 2 - add basic files

---
+++ lib/db/database_helper.dart
@@ -0,0 +1,57 @@
+import 'package:justdoit/models/task.dart';
+import 'package:sqflite/sqflite.dart';
+import 'package:path/path.dart';
+
+class DatabaseHelper {
+  static final DatabaseHelper _instance = DatabaseHelper._internal();
+  factory DatabaseHelper() => _instance;
+
+  DatabaseHelper._internal();
+
+  Database? _database;
+
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase();
+    return _database!;
+  }
+
+  Future<Database> _initDatabase() async {
+    String path = join(await getDatabasesPath(), 'tasks.db');
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: (db, version) {
+        return db.execute(
+          'CREATE TABLE tasks(id TEXT PRIMARY KEY, title TEXT, isCompleted INTEGER, createdAt TEXT)',
+        );
+      },
+    );
+  }
+
+  Future<void> insertTask(Task task) async {
+    final db = await database;
+    await db.insert(
+      'tasks',
+      task.toMap(),
+      conflictAlgorithm: ConflictAlgorithm.replace,
+    );
+  }
+
+  Future<List<Task>> getTasks() async {
+    final db = await database;
+    final List<Map<String, dynamic>> maps = await db.query('tasks');
+    return List.generate(maps.length, (i) {
+      return Task.fromMap(maps[i]);
+    });
+  }
+
+  Future<void> deleteTask(String id) async {
+    final db = await database;
+    await db.delete(
+      'tasks',
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+  }
+}
---
+++ lib/models/task.dart
@@ -0,0 +1,33 @@
+class Task {
+  final String id;
+  final String title;
+  final bool isCompleted;
+  final DateTime createdAt;
+
+  Task({
+    required this.id,
+    required this.title,
+    this.isCompleted = false,
+    required this.createdAt,
+  });
+
+  Map<String, dynamic> toMap() {
+  return {
+    'id': id,
+    'title': title,
+    'isCompleted': isCompleted,
+    'createdAt': createdAt.toIso8601String(),//преобразовалка в красивую дату
+  };
+}
+
+factory Task.fromMap(Map<String, dynamic> map) {
+  return Task(
+    id: map['id'],
+    title: map['title'],
+    isCompleted: map['isCompleted'] == 1,
+    createdAt: DateTime.parse(map['createdAt']),
+  );
+}
+
+}
+
